2e7435d90f8922c6a28ac7054e6f2d85e7096920
Pedro Everett <pedrojoseeverett@gmail.com>
2017-05-23 17:44:34 +0100

    test_number_to_short_name passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -23,12 +23,30 @@ def length_of_string(string)
   return string.length()
 end
 
-def join_string (string1, string2)
+def join_string(string1, string2)
   return string1 + string2
 end
 
-def add_string_as_number (string1, string2)
+def add_string_as_number(string1, string2)
    return string1.to_i() + string2.to_i()
 end
 
+def number_to_full_month_name(int)
+  case int
+  when  1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  end
+
+end
+
+
+def number_to_short_month_name(int)
+  #call the function above, then get the first 3 letter out
+  long_name = number_to_full_month_name(int)
+  return long_name[0..2]
+end
 
